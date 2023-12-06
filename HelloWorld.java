5b975bc8eeaff83580ffb4e6ee84bc707f4814f2
guiji2007 <98149176+guiji2007@users.noreply.github.com>
2023-12-06 11:47:28 -0600

    exercise2 complete

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,39 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World");
+        String myStr ="a";
+        System.out.println(myStr);
+
+        int myFavoriteNumber = 7;
+        System.out.println(myFavoriteNumber);
+        String myString = "b";
+        System.out.println(myString);
+        float nyNumber = 3.14f;
+        System.out.println(nyNumber);
+
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+        int myNumber = Integer.parseInt("3");
+        System.out.println(myNumber);
+
+//        int x = 4;
+//        x+=5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+        System.out.println("x is " + x + "y is " + y);
+
+
     }
 }
